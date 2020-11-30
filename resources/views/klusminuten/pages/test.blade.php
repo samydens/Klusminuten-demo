73624d6a2986d2b271fac898ff3011b7df90375d
Sam Ydens <sam.ydens@gmail.com>
2020-11-30 15:11:45 +0100

    test view
    test view

--- resources/views/klusminuten/pages/test.blade.php
+++ resources/views/klusminuten/pages/test.blade.php
@@ -1,10 +1,11 @@
 @extends('klusminuten.layout.app')
 
 @section('content')
+    <a href="/dashboard">Dashboard</a>
     <div class="container sm mx-auto">
-        <p class="font-title font-bold text-5xl">Font test pagina</p>
+        <p class="font-title font-bold text-5xl text-kmorange">Font test pagina</p>
 
-        <p class="font-title text-3xl">ubuntu</p>
+        <p class="font-title text-3xl text-kmorange">ubuntu</p>
         <p class="font-title text-3xl font-medium">ubuntu medium</p>
         <p class="font-title text-3xl font-bold">ubuntu bold</p>
         <p class="font-par text-3xl">roboto</p>
--- resources/views/navigation-dropdown.blade.php
+++ resources/views/navigation-dropdown.blade.php
@@ -15,6 +15,9 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
+                    <x-jet-nav-link href="/test">
+                        {{ __('Test') }}
+                    </x-jet-nav-link>
                 </div>
             </div>
 
