7ed88e3455831b77045474eaa8ff189407e824e1
Sam Ydens <sam.ydens@gmail.com>
2020-11-30 10:46:13 +0100

    Test page
    For testing the added fonts and colors

---
+++ resources/views/klusminuten/pages/test.blade.php
@@ -0,0 +1,14 @@
+@extends('klusminuten.layout.app')
+
+@section('content')
+    <div class="container sm mx-auto">
+        <p class="font-title font-bold text-5xl">Font test pagina</p>
+
+        <p class="font-title text-3xl">ubuntu</p>
+        <p class="font-title text-3xl font-medium">ubuntu medium</p>
+        <p class="font-title text-3xl font-bold">ubuntu bold</p>
+        <p class="font-par text-3xl">roboto</p>
+        <p class="font-par text-3xl font-medium">roboto medium</p>
+        <p class="font-par text-3xl font-bold">roboto bold</p>
+    </div>
+@endsection
