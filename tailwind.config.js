c157eb156a5402a57367ab63732c5d7b95598fdb
Sam Ydens <sam.ydens@gmail.com>
2020-11-30 14:34:29 +0100

    tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,20 +8,32 @@ module.exports = {
     ],
 
     theme: {
+        colors: {
+            text: {
+                orange: '#FF6600',
+                darkgray: '#404040',
+                lightgray: '#f2f1f1',
+                bluegray: '#99A2AB',
+            },
+
+            input: {
+                background: '#E4E4E4',
+                border: '#707070',
+            },
+
+            gradient: {
+                graystart: '#E9E9E9',
+                grayend: '#F1F1F1',
+                orangestart: '#FF6600',
+                orangeend: '##FB7F21'
+            },
+        },
+
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-                title: ['Ubuntu', ...defaultTheme.fontFamily.sans],
-                par: ['Roboto', ...defaultTheme.fontFamily.sans],
-            },
-
-            colors: {
-                kmorange: '#ff6600',
-                kmtitel: '#404040',
-                kmbackground: '#f2f1f1',
-                kmparagraph: '#99A2AB',
-                kminputbg: '#E4E4E4',
-                kminputborder: '#707070',
+                ubuntu: ['Ubuntu', ...defaultTheme.fontFamily.sans],
+                roboto: ['Roboto', ...defaultTheme.fontFamily.sans],
             },
         },
     },
