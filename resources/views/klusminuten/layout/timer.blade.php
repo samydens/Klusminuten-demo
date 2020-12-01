11bfd72262b3ab56ba8415ed8a48fb234269591a
Sam Ydens <sam.ydens@gmail.com>
2020-12-01 13:20:16 +0100

    Timer layout
    Layout file for timer and material cost views

---
+++ resources/views/klusminuten/layout/timer.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet">
+
+        @livewireStyles
+
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
+    </head>
+    <body class="antialiased bg-white w-screen h-screen overflow-hidden">
+        @yield('header')
+        @yield('content')
+        @livewireScripts
+    </body>
+</html>
