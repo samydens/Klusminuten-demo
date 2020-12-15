42031c98246aa6d969e14341ecc6ee90c8b8a88d
Sam Ydens <sam.ydens@gmail.com>
2020-12-15 16:08:44 +0100

    Lazy model, image upload field

--- resources/views/livewire/add-client.blade.php
+++ resources/views/livewire/add-client.blade.php
@@ -4,22 +4,22 @@
     
     <form wire:submit.prevent="submit" class="flex flex-col space-y-4">
         
-        <label>Volledige naam<input type="text" wire:model="client.full_name" class="border-gray-400 border bg-gray-200 rounded w-full h-9 py-1 px-4"></label>
+        <label>Volledige naam<input type="text" wire:model.lazy="client.full_name" class="border-gray-400 border bg-gray-200 rounded w-full h-9 py-1 px-4"></label>
         @error('client.full_name') <span class="text-red text-xs">{{ $message }}</span><br> @enderror
         
-        <label>Adres<input type="text" wire:model="client.adres" class="border-gray-400 border bg-gray-200 rounded w-full h-9 py-1 px-4"></label>
+        <label>Adres<input type="text" wire:model.lazy="client.adres" class="border-gray-400 border bg-gray-200 rounded w-full h-9 py-1 px-4"></label>
         @error('client.adres') <span class="text-red text-xs">{{ $message }}</span><br> @enderror
         
-        <label>Postcode<input type="text" wire:model="client.zip" class="border-gray-400 border bg-gray-200 rounded w-full h-9 py-1 px-4"></label>
+        <label>Postcode<input type="text" wire:model.lazy="client.zip" class="border-gray-400 border bg-gray-200 rounded w-full h-9 py-1 px-4"></label>
         @error('client.zip') <span class="text-red text-xs">{{ $message }}</span><br> @enderror
         
-        <label>Woonplaats<input type="text" wire:model="client.city" class="border-gray-400 border bg-gray-200 rounded w-full h-9 py-1 px-4"></label>
+        <label>Woonplaats<input type="text" wire:model.lazy="client.city" class="border-gray-400 border bg-gray-200 rounded w-full h-9 py-1 px-4"></label>
         @error('client.city') <span class="text-red text-xs">{{ $message }}</span><br> @enderror
         
-        <label>Telefoonnummer<input type="number" wire:model="client.phone_number" class="border-gray-400 border bg-gray-200 rounded w-full h-9 py-1 px-4"></label>
+        <label>Telefoonnummer<input type="number" wire:model.lazy="client.phone_number" class="border-gray-400 border bg-gray-200 rounded w-full h-9 py-1 px-4"></label>
         @error('client.phone_number') <span class="text-red text-xs">{{ $message }}</span><br> @enderror
         
-        <label>E-mail<input type="email" wire:model="client.mail" class="border-gray-400 border bg-gray-200 rounded w-full h-9 py-1 px-4"></label>
+        <label>E-mail<input type="email" wire:model.lazy="client.mail" class="border-gray-400 border bg-gray-200 rounded w-full h-9 py-1 px-4"></label>
         @error('client.mail') <span class="text-red text-xs">{{ $message }}</span><br> @enderror
         
         <div class="flex flex-col space-y-4 text-center">
