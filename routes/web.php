dd743119a1fc44528a8dc4f8eb8dac434835083a
Sam Ydens <sam.ydens@gmail.com>
2021-01-04 15:44:23 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,40 +19,18 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 })->name('dashboard');
 
 
- // Check if logged in
-route::middleware('auth')->group( function() {
-    
-    // Edit users
-    Route::group(['middleware' => ['permission:edit users']], function () {
-        Route::get('/admin/user', App\Http\Livewire\UserAdmin::class)->name('admin/user'); 
-        Route::get('/admin/user/{id}', App\Http\Livewire\EditUser::class)->name('admin/user/edit'); 
-    });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-    // Edit jobs
-    Route::group(['middleware' => ['permission:edit jobs']], function () {
-        Route::get('/admin/klus', App\Http\Livewire\JobAdmin::class)->name('admin/job'); 
-        Route::get('/admin/klus/{id}', App\Http\Livewire\Editjob::class)->name('admin/job/edit'); // Edit page
-        Route::get('/resklus', App\Http\Livewire\ResponsiveJobAdmin::class);
-        
-        Route::get('/testadmin', function () {
-            return view('testadmin');
-        });
-    });
-    
-    // Accesible without roles or permissions
-    Route::get('/', function () { 
-        return redirect('/home'); // redirect to /home
-    }); 
+Route::get('/home', function () {
+    return view('pages.home');
+});
 
-    Route::get('/toevoegen', function () {
-        return view('klusminuten.pages.addjob'); // Add page
-    }); 
+Route::get('/toevoegen', function () {
+    return view('pages.add');
+});
 
-    Route::get('/chooseemployee', App\Http\Livewire\ChooseEmployee::class); // Choose employee
-    Route::get('/home', App\Http\Livewire\CurrentJobs::class); // Dashboard
-    Route::get('/klusvijver', App\Http\Livewire\JobIndex::class); // Klusvijver
-    Route::get('/klusvijver/{id}', App\Http\Livewire\showJob::class); // Show job
-    Route::get('/stopwatch/{id}', App\Http\Livewire\NewTimer::class); // Stopwatch
-    Route::get('/materiaal/{id}', App\Http\Livewire\AddMaterial::class); // Materials
-    Route::get('/archief', App\Http\Livewire\Archive::class); // Archive
+Route::get('/klusvijver', function() {
+    return view('pages.klusvijver');
 });
