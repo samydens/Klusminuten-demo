308b562fece7baad4dc8e95cd94617835b671f53
Sam Ydens <sam.ydens@gmail.com>
2021-01-05 10:54:43 +0100

    routing

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('title')
+    Dashboard
+@endsection
+
+@section('content')
+    <livewire:current-jobs>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,19 +18,52 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
-
+// welcome page
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.welcome');
 });
 
+// dashboard
 Route::get('/home', function () {
     return view('pages.home');
 });
 
+// add jobs, employees, customers
 Route::get('/toevoegen', function () {
     return view('pages.add');
 });
 
+// klusvijver
 Route::get('/klusvijver', function() {
     return view('pages.klusvijver');
+});
+
+// archief
+Route::get('/klusvijver/archief', function () {
+    return view('pages.archief');
+});
+
+// user admin
+Route::get('/admin/gebruikers', function() {
+    return view('pages.admin.user');
+});
+
+// job admin
+Route::get('/admin/klus', function () {
+    return view('pages.admin.job');
+});
+
+// admin home
+Route::get('/admin', function () {
+    return view('pages.admin.admin');
+});
+
+// timer
+Route::get('/timer/{id}', function () {
+    return view('timer');
+});
+
+// material
+Route::get('/material/{id}', function () {
+    return view('material');
 });
