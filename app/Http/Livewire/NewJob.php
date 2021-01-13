63bf83addd604b9959bb652489b72d0773391902
Sam Ydens <sam.ydens@gmail.com>
2021-01-13 11:57:16 +0100

    update

--- app/Http/Livewire/NewJob.php
+++ app/Http/Livewire/NewJob.php
@@ -13,15 +13,19 @@ class NewJob extends Component
 {
     use WithFileUploads;
 
-    public $step, $job, $client, $employee, $customerIndex, $employeeIndex, $photo, $employeeIds;
+    public $step, $client, $employee;
+    public $job, $photo;
+    public $selectEmp = [];
+
+    public $customerIndex, $employeeIndex;
 
     private $stepActions = [
-        'submit1', // step 0
-        'submit2', // step 1
-        'submit3', // step 2
-        'submit4', // step 3
-        'submitClient', // step 4 (step 1)
-        'submitEmployee' // step 5 (step 2)
+        'submit0', // Title, desc & photo.
+        'submit1', // Client.
+        'submit2', // Employee
+        'submit3', // Agreements (submit job to DB)
+        'submit4', // new client
+        'submit5', // new customer
     ];
 
     protected $messages = [
@@ -36,141 +40,128 @@ class NewJob extends Component
     public function mount()
     {
         $this->step = 0;
-
-        // Get all clients
         $this->customerIndex = Client::all();
-
-        // Get all employee's
         $this->employeeIndex = Employee::all();
-    }
-
-    public function previousStep()
-    {
-        // decrement step
-        $this->step--;
     }
 
     public function submit()
     {
-        // get the submit function belonging to current step
+        // Get the submit function belonging to current step.
         $action = $this->stepActions[$this->step];
 
-        // execute function
+        // Execute function.
         $this->$action();
+    }
+
+    public function previousStep()
+    {
+        $this->step--;
     }
 
     public function updatedphoto()
     {
-        // Check if image is valid when uploaded.
+        // Live image validation
         $this->validate([
             'photo' => 'max:2048|mimes:jpg,png,jpeg|nullable'
         ]);
     }
 
-    public function submit1()
+    public function submit0()
     {
-        // validate input.
         $this->validate([
             'job.title' => 'required|max:255',
             'job.desc' => 'required',
             'photo' => 'max:2048|mimes:jpg,png,jpeg|nullable'
         ]);
 
-        // if photo is set then use store it, else use standard image for jobs.
+        // Set standard image if no image is uploaded.
         if ($this->photo) {
             $this->job['photo_url'] = $this->photo->store('photos', 'public');
         } else {
             $this->job['photo_url'] = 'photos/bathroom.jpg';
         }
 
-        // Increment step
+        $this->step++;
+    }
+
+    public function submit1()
+    {
+        $this->validate([
+            'job.client_id' => 'required'
+        ]);
+
+        $client = Client::find($this->job['client_id']);
+        $this->job['location'] = $client->city;
+
         $this->step++;
     }
 
     public function submit2()
     {
-        // validate input
-        $this->validate([
-            'job.client_name' => 'required|max:255'
-        ]);
-
-        // Get client ID by name.
-        $client = Client::where('full_name', '=', $this->job['client_name'])->first();
-        $this->job['client_id'] = $client->id;
-
-        // Get location via client
-        $this->job['location'] = $client->city;
-
-        // Increment step.
+        // Hier komt nog validation.
         $this->step++;
+    }
+
+    public function addField()
+    {
+        // Add a new employee field.
+        $this->selectEmp[] = '';
+    }
+
+    public function deleteFieldById($id)
+    {
+        unset($this->selectEmp[$id]);
     }
 
     public function submit3()
     {
-        // validate input.
-        $this->validate([
-            'job.employee_name' => 'required|max:255'
-        ]);
-
-        // Get employee ID by name.
-        $employee = Employee::where('name', '=', $this->job['employee_name'])->first();
-        $this->job['employee_id'] = $employee->id;
-
-        // Increment step.
-        $this->step++;
-    }
-
-    public function submit4()
-    {
-        // Validate form input.
         $this->validate([
             'job.agr_minutes' => 'required|max:9999|integer',
             'job.agr_material' => 'required|max:999999.99'
         ]);
-        
-        // Save new job to database.
-        $job = new Job;
-        $job->title = $this->job['title'];
-        $job->desc = $this->job['desc'];
-        $job->photo = $this->job['photo_url'];
-        $job->location = $this->job['location'];
-        $job->user_id = Auth::id();
-        $job->client_id = $this->job['client_id'];
-        $job->agr_minutes = $this->job['agr_minutes'];
-        $job->agr_material = $this->job['agr_material'];
-        $job->save();
 
-        // // Save relation between job and employee.
-        // $job->employees()->attach($this->job['employee_id']);
+        $job = Job::create([
+            'title' => $this->job['title'],
+            'desc' => $this->job['desc'],
+            'photo' => $this->job['photo_url'],
+            'location' => $this->job['location'],
+            'user_id' => Auth::id(),
+            'client_id' => $this->job['client_id'],
+            'agr_minutes' => $this->job['agr_minutes'],
+            'agr_material' => $this->job['agr_material']
+        ]);
 
-        foreach ($this->employeeIds as $employeeId) {
-            $job->employees()->attach($employeeId);
+        foreach($this->selectEmp as $employee) {
+            $job->employees()->attach($employee);
         }
-        // Set step to 0.
-        $this->step = 0;
 
-        // Empty all modeled variables.
-        $this->job = '';
-        $this->employee = '';
-        $this->client = '';
-        $this->photo = '';
+        // unset($this->job);
+        // unset($this->photo);
+        // unset($this->selectEmp);
+
+        $this->reset(['job', 'photo', 'selectEmp']);
+
+        $this->step = 0;
+        
+        // return redirect()->to('/home');
     }
 
     public function newClient()
     {
-        // Set step to 4 (add new client.)
+        // Sets step to 4 (new client form.)
         $this->step = 4;
     }
     
     public function newEmployee()
     {
-        // Set step to 5 (add new employee.)
+        // Sets step to 5 (add new employee.)
         $this->step = 5;
     }
 
-    public function submitClient()
+    public function submit4()
     {
-        // Validate input.
+        $client = $this->client;
+
         $this->validate([
             'client.full_name' => 'required|max:255',
             'client.adres' => 'required|max:255',
@@ -179,54 +170,47 @@ class NewJob extends Component
             'client.mail' => 'required|email'
         ]);
 
-        // Makes code more readable.
-        $client = $this->client;
-        
-        // New client object.
-        $newClient  = new Client;
+        $client = Client::create([
+            'full_name' => $client['full_name'],
+            'adres' => $client['adres'],
+            'zip' => $client['zip'],
+            'city' => $client['city'],
+            'phone_number' => $client['client_phone'],
+            'mail' => $client['mail'],
+        ]);
 
-        // Add to client object.
-        $newClient->full_name = $client['full_name'];
-        $newClient->adres = $client['adres'];
-        $newClient->zip = $client['zip'];
-        $newClient->city = $client['city'];
-        $newClient->phone_number = $client['phone_number'];
-        $newClient->mail = $client['mail'];
+        $this->job['client_id'] = $client->id;
+        $this->job['location'] = $client->city;
 
-        // Save client object to DB.
-        $newClient->save();
+        // unset($this->client);
+        $this->reset('client');
 
-        // These are getting stored in the job table when job is submitted to DB.
-        $this->job['client_id'] = $newClient->id;
-        $this->job['location'] = $newClient->city;
-
-        // Set step to 2 (next step)
         $this->step = 2;
     }
 
-    public function submitEmployee()
+    public function submit5()
     {
-        // Validate input
+        $employee = $this->employee;
+
         $this->validate([
             'employee.name' => 'required',
             'employee.vakman_id' => 'numeric|required',
+            'employee.employee_phone' => 'numeric|required',
+        ]);
+        
+        $employee = Employee::create([
+            'name' => $employee['name'],
+            'vakman_id' => $employee['vakman_id'],
+            'phone_number' => $employee['employee_phone']
         ]);
 
-        // Add to employee object
-        $employee = $this->employee;
-        $newEmployee = new Employee;
-        $newEmployee->name = $employee['name'];
-        $newEmployee->vakman_id = $employee['vakman_id'];
-        $newEmployee->phone_number = $employee['phone'];
+        $this->employeeIndex = Employee::all();
+        $this->selectEmp[] = strval($employee->id);
+        
+        // unset($this->employee);
+        $this->reset('employee');
 
-        // Save employee object to DB
-        $newEmployee->save();
-
-        // Assign id of new employee to id thats going to be stored in job
-        $this->job['employee_id'] = $newEmployee->id;
-
-        // Set step to 3 (next step)
-        $this->step = 3;
+        $this->step = 2;
     }
 
     public function backFromNew()
--- routes/web.php
+++ routes/web.php
@@ -36,18 +36,13 @@ route::middleware('auth')->group( function() {
     Route::group(['middleware' => ['permission:edit jobs']], function () {
         Route::get('/admin/klus', App\Http\Livewire\JobAdmin::class)->name('admin/job'); 
         Route::get('/admin/klus/{id}', App\Http\Livewire\Editjob::class)->name('admin/job/edit'); // Edit page
-        // Route::get('/admin/klussers', App\Http\Livewire\EmployeeAdmin::class)->name('admin/klusser'); // Employee admin page
-        // Route::get('/resklus', App\Http\Livewire\ResponsiveJobAdmin::class);
         
         Route::get('/testadmin', function () {
             return view('testadmin');
         });
     });
     
-    Route::get('/toevoegen', function () {
-        return view('klusminuten.pages.addjob'); // Add page
-    }); 
-
+    Route::get('/toevoegen', App\Http\Livewire\NewJob::class); // Create a new job.
     Route::get('/home', App\Http\Livewire\CurrentJobs::class); // Dashboard
     Route::get('/klusvijver', App\Http\Livewire\JobIndex::class); // Klusvijver
     Route::get('/klusvijver/{id}', App\Http\Livewire\showJob::class); // Show job
@@ -56,6 +51,4 @@ route::middleware('auth')->group( function() {
     Route::get('/archief', App\Http\Livewire\Archive::class); // Archive
     Route::get('/klus/toevoegen', App\Http\Livewire\NewJob::class); // new job test link
     Route::get('/klus/werknemers', App\Http\Livewire\EmployeeJobs::class); // test for employeejob relation
-    Route::get('/test/1', App\Http\Livewire\Multipleemployeetest::class);
-    
 });
